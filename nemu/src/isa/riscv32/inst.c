eae261fbbd3e4eb473696114049bde529fda3a97
tracer-ics2025 <tracer@njuics.org>
2025-09-17 19:54:30 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-79-generic #79~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Aug 15 16:54:53 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:54:30 up  1:45,  1 user,  load average: 0.99, 1.20, 1.11

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -23,15 +23,18 @@
 #define Mw vaddr_write
 
 enum {
-  TYPE_I, TYPE_U, TYPE_S,
+  TYPE_I, TYPE_U, TYPE_S, TYPE_J,
   TYPE_N, // none
 };
 
 #define src1R() do { *src1 = R(rs1); } while (0)
 #define src2R() do { *src2 = R(rs2); } while (0)
-#define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
-#define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
-#define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immI()  do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
+#define immU()  do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
+#define immS()  do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
+#define immJ()  do { \
+  *imm = SEXT((BITS(i,31,31)<<20) | (BITS(i,19,12)<<12) | (BITS(i,20,20)<<11) | (BITS(i,30,21)<<1), 21); \
+} while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst;
@@ -42,6 +45,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_J:                   immJ(); break;
     case TYPE_N: break;
     default: panic("unsupported type = %d", type);
   }
@@ -59,10 +63,14 @@ static int decode_exec(Decode *s) {
 }
 
   INSTPAT_START();
+  // 基础指令实现
   INSTPAT("??????? ????? ????? ??? ????? 00101 11", auipc  , U, R(rd) = s->pc + imm);
+  INSTPAT("0000000 ????? ????? 000 ????? 00100 11", addi   , I, R(rd) = src1 + imm);
+  INSTPAT("??????? ????? ????? 000 ????? 11001 11", jalr   , I, { word_t t = s->snpc; s->dnpc = (src1 + imm) & ~1; R(rd) = t; });
+  INSTPAT("??????? ????? ????? ??? ????? 11011 11", jal    , J, { word_t t = s->snpc; s->dnpc = s->pc + imm; R(rd) = t; });
+  INSTPAT("??????? ????? ????? 010 ????? 01000 11", sw     , S, Mw(src1 + imm, 4, src2));
   INSTPAT("??????? ????? ????? 100 ????? 00000 11", lbu    , I, R(rd) = Mr(src1 + imm, 1));
   INSTPAT("??????? ????? ????? 000 ????? 01000 11", sb     , S, Mw(src1 + imm, 1, src2));
-
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
