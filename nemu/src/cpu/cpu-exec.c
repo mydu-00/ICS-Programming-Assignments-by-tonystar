cacbb382d648b57475e5efa5f7ca4bc6a974a2d9
tracer-ics2025 <tracer@njuics.org>
2025-09-23 15:38:32 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:38:32 up 53 min,  1 user,  load average: 0.97, 0.59, 0.48

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -137,6 +137,7 @@ void cpu_exec(uint64_t n) {
     case NEMU_RUNNING: nemu_state.state = NEMU_STOP; break;
 
     case NEMU_END: case NEMU_ABORT:
+      iringbuf_dump(); /* print recent instruction ring buffer on failure */
       Log("nemu: %s at pc = " FMT_WORD,
           (nemu_state.state == NEMU_ABORT ? ANSI_FMT("ABORT", ANSI_FG_RED) :
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
