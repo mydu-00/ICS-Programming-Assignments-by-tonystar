c67d9c5f5552d0a97268ae7860f0f5427b60bbb4
tracer-ics2025 <tracer@njuics.org>
2025-09-23 21:56:09 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:56:09 up 28 min,  1 user,  load average: 0.59, 0.59, 0.54

--- abstract-machine/scripts/platform/nemu.mk
+++ abstract-machine/scripts/platform/nemu.mk
@@ -34,6 +34,7 @@ image: image-dep
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
+# 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).bin
 	@echo "+ invoking NEMU with ELF (for ftrace/symbols)"
 	$(MAKE) -C $(NEMU_HOME) ISA=$(ISA) run ARGS="$(NEMUFLAGS)" IMG=$(IMAGE).elf
 
