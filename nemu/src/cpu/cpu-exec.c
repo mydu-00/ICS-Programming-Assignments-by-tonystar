5743b781f9cfff44de5183fd43eeba47d24a4a7e
tracer-ics2025 <tracer@njuics.org>
2025-09-23 15:37:47 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-83-generic #83~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 18:19:47 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:37:47 up 52 min,  1 user,  load average: 0.96, 0.53, 0.46

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -107,7 +107,6 @@ static void statistic() {
 void assert_fail_msg() {
   isa_reg_display();
 #ifdef CONFIG_ITRACE
-  fprintf(stderr, "ASSERT_FAIL_MSG called, dumping iringbuf...\n");
   iringbuf_dump(); /* print recent instruction ring buffer on failure */
 #endif
   statistic();
