0e9bc1266d32c4ddb5c2eee3f82147b23665401b
tracer-ics2025 <tracer@njuics.org>
2025-09-28 11:01:45 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-84-generic #84~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 14:29:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:01:45 up 28 min,  1 user,  load average: 0.80, 0.65, 0.65

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -2,9 +2,27 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
+#define KEY_QUEUE_LEN 1024
+
+static int key_queue[KEY_QUEUE_LEN] = {};
+static int key_f = 0, key_r = 0;
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
-  uint32_t data = inl(KBD_ADDR);
-  kbd->keydown = (data & KEYDOWN_MASK) != 0;
-  kbd->keycode = data & ~KEYDOWN_MASK;
+  // 先把 NEMU 的 i8042 事件全部读入本地队列
+  while (1) {
+    uint32_t data = inl(KBD_ADDR);
+    if ((data & ~KEYDOWN_MASK) == 0) break; // AM_KEY_NONE
+    key_queue[key_r] = data;
+    key_r = (key_r + 1) % KEY_QUEUE_LEN;
+    // 防止溢出
+    if (key_r == key_f) key_f = (key_f + 1) % KEY_QUEUE_LEN;
+  }
+
+  uint32_t k = AM_KEY_NONE;
+  if (key_f != key_r) {
+    k = key_queue[key_f];
+    key_f = (key_f + 1) % KEY_QUEUE_LEN;
+  }
+  kbd->keydown = (k & KEYDOWN_MASK) != 0;
+  kbd->keycode = k & ~KEYDOWN_MASK;
 }
