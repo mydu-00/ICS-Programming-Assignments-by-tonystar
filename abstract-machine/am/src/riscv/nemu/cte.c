6a696282d544b4cb21bb6a3c97e336f91cfc5e79
tracer-ics2025 <tracer@njuics.org>
2025-10-02 18:40:54 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-84-generic #84~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 14:29:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:40:54 up  3:26,  1 user,  load average: 0.38, 0.79, 0.84

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -37,6 +37,7 @@ Context* __am_irq_handle(Context *c) {
 extern void __am_asm_trap(void);
 
 bool cte_init(Context*(*handler)(Event, Context*)) {
+  printf("mtvec set to %p\n", __am_asm_trap);
   // initialize exception entry
   asm volatile("csrw mtvec, %0" : : "r"(__am_asm_trap));
 
