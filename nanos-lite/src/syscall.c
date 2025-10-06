309237b2bd1f1fe655875c9dcb2f82fe15986c1b
tracer-ics2025 <tracer@njuics.org>
2025-10-06 21:10:24 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:10:24 up  1:41,  1 user,  load average: 0.68, 0.70, 0.80

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -3,31 +3,65 @@
 
 #include "syscall.h"
 
+#ifdef CONFIG_STRACE
+#ifdef CONFIG_STRACE_TO_LOG
+extern void log_write(const char *fmt, ...);
+#define STRACE_PRINT(...) log_write(__VA_ARGS__)
+#else
+#define STRACE_PRINT(...) printf(__VA_ARGS__)
+#endif
+
+static const char *sys_name(uintptr_t id) {
+  switch (id) {
+    case SYS_exit:  return "exit";
+    case SYS_yield: return "yield";
+    default:        return "unknown";
+  }
+}
+#endif
+
 /* perform syscall handling; set return value via c->GPRx */
 void do_syscall(Context *c) {
   uintptr_t id   = c->GPR1;
   uintptr_t arg0 = c->GPR2;
-  uintptr_t arg1 = c->GPR3;
-  uintptr_t arg2 = c->GPR4;
+  // uintptr_t arg1 = c->GPR3;
+  // uintptr_t arg2 = c->GPR4;
 
-  printf("[do_syscall] id=%u raw(GPR1)=0x%x a0=%u a1=%u a2=%u\n",
-         (unsigned)id, (unsigned)c->GPR1,
-         (unsigned)arg0, (unsigned)arg1, (unsigned)arg2);
-
+#ifdef CONFIG_STRACE
+  const char *name = sys_name(id);
+  // 打印调用头部
   if (id == SYS_exit) {
-    printf("[diag] exit status=%u\n", (unsigned)arg0);
+    STRACE_PRINT("[strace] %s(%d)", name, (int)arg0);
+  } else if (id == SYS_yield) {
+    STRACE_PRINT("[strace] %s()", name);
+  } else {
+    STRACE_PRINT("[strace] %s(%u,%u,%u)", name,
+                 (unsigned)arg0, (unsigned)arg1, (unsigned)arg2);
   }
+#endif
 
   switch (id) {
-    case SYS_yield:         
+    case SYS_yield:
       c->GPRx = 0;
       break;
 
     case SYS_exit:
-      halt((int)arg0); 
+#ifdef CONFIG_STRACE
+      STRACE_PRINT(" = ? <halt>\n");
+#endif
+      halt((int)arg0);
       break;
 
     default:
+#ifdef CONFIG_STRACE
+      STRACE_PRINT(" = ? <panic>\n");
+#endif
       panic("Unhandled syscall ID = %u", (unsigned)id);
   }
+
+#ifdef CONFIG_STRACE
+  if (id != SYS_exit) {
+    STRACE_PRINT(" = %ld\n", (long)c->GPRx);
+  }
+#endif
 }
