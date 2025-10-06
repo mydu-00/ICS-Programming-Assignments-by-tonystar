8d33726e5ff8cd242dda2c6435f9ef5cf22f9743
tracer-ics2025 <tracer@njuics.org>
2025-10-06 20:01:13 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:01:13 up 31 min,  1 user,  load average: 0.93, 0.96, 0.83

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,30 +1,43 @@
-#include <memory.h>
-
+#include <common.h>
+#include "/home/tony/codingproj/ics2025/abstract-machine/am/include/am.h"
+// #include <nemu.h>
+#define PGSIZE    4096
 static void *pf = NULL;
+static uintptr_t brk_curr = 0;  // 记录当前 program break
 
 void* new_page(size_t nr_page) {
-  return NULL;
+  void *ret = pf;
+  pf = (void *)((uintptr_t)pf + nr_page * PGSIZE);
+  return ret;
 }
 
 #ifdef HAS_VME
 static void* pg_alloc(int n) {
-  return NULL;
+  return new_page((n + PGSIZE - 1) / PGSIZE);
 }
 #endif
 
 void free_page(void *p) {
-  panic("not implement yet");
+  panic("free_page() not implemented");
 }
 
-/* The brk() system call handler. */
 int mm_brk(uintptr_t brk) {
+  if (brk == 0) return 0;
+  if (brk_curr == 0) brk_curr = (uintptr_t)pf;
+  if (brk > brk_curr) {
+    // 需要分配 (brk - brk_curr) 向上取整的页
+    uintptr_t need = brk - brk_curr;
+    size_t pages = (need + PGSIZE - 1) / PGSIZE;
+    new_page(pages);
+    brk_curr = brk;
+  }
   return 0;
 }
 
 void init_mm() {
   pf = (void *)ROUNDUP(heap.start, PGSIZE);
+  brk_curr = (uintptr_t)pf;
   Log("free physical pages starting from %p", pf);
-
 #ifdef HAS_VME
   vme_init(pg_alloc, free_page);
 #endif
