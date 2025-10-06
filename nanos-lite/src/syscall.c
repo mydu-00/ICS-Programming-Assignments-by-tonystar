3c3d3c2704ca95150657c277b2a08171942829b4
tracer-ics2025 <tracer@njuics.org>
2025-10-06 17:17:37 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:17:37 up  3:59,  1 user,  load average: 0.80, 0.74, 0.63

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -11,24 +11,25 @@
 
 /* perform syscall handling; set return value via c->GPRx */
 void do_syscall(Context *c) {
-  uintptr_t id = c->GPR1;
+  uintptr_t id   = c->GPR1;
   uintptr_t arg0 = c->GPR2;
   uintptr_t arg1 = c->GPR3;
   uintptr_t arg2 = c->GPR4;
 
-  printf("[do_syscall] id=%u raw(a5)=0x%x a0=%u a1=%u a2=%u\n",
+  printf("[do_syscall] id=%u raw(GPR1)=0x%x a0=%u a1=%u a2=%u\n",
          (unsigned)id, (unsigned)c->GPR1,
          (unsigned)arg0, (unsigned)arg1, (unsigned)arg2);
 
   if (id == 0) {
-    printf("[diag] gpr[15](a5)=%x gpr[17](a7)=%x (both shown to检测混配)\n",
+    printf("[diag] gpr[15](a5)=%x gpr[17](a7)=%x\n",
            (unsigned)c->gpr[15],
            (unsigned)((NR_REGS > 17) ? c->gpr[17] : 0));
   }
 
   switch (id) {
     case SYS_yield:
-      yield();
+      // 关键修复：不要再次调用 yield() 以避免嵌套 ecall 和双重 mepc +=4
+      // 如果有调度器，可在这里调用 schedule();
       c->GPRx = 0;
       break;
     case SYS_exit:
