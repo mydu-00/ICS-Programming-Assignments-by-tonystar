346d5cfa3799692837db28c4ee8dbc724c756b89
tracer-ics2025 <tracer@njuics.org>
2025-10-06 16:19:24 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:19:24 up  3:01,  1 user,  load average: 0.61, 0.37, 0.31

--- navy-apps/scripts/riscv32e.mk
+++ navy-apps/scripts/riscv32e.mk
@@ -1,3 +1,4 @@
 include $(NAVY_HOME)/scripts/riscv/common.mk
-CFLAGS  += -march=rv32em -mabi=ilp32e  #overwrite
+# Use COMMON_CFLAGS from top-level to keep ISA/ABI consistent across projects
+CFLAGS  += $(COMMON_CFLAGS)
 LDFLAGS += -melf32lriscv
