56a3bccf207715c2044d3c8dacd5c2a49177db02
tracer-ics2025 <tracer@njuics.org>
2025-10-08 21:40:28 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:40:28 up  2:56,  1 user,  load average: 0.85, 0.74, 0.63

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -15,7 +15,7 @@ typedef struct {
   WriteFn write;
 } Finfo;
 
-enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
+enum { FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB };
 
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
@@ -27,9 +27,9 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
   return 0;
 }
 
-/* This is the information about all files in disk. */
+/* 文件记录表: 保留前三个占位项 */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
+  [FD_STDIN]  = {"stdin",  0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
 #include "files.h"
@@ -38,17 +38,16 @@ static Finfo file_table[] __attribute__((used)) = {
 enum { NR_FILES = sizeof(file_table) / sizeof(file_table[0]) };
 static size_t file_offset[NR_FILES];
 
-void init_fs() {
+void init_fs(void) {
   memset(file_offset, 0, sizeof(file_offset));
 }
 
 int fs_open(const char *pathname, int flags, int mode) {
-  (void)flags;
-  (void)mode;
-  for (size_t i = 0; i < NR_FILES; i++) {
-    if (strcmp(file_table[i].name, pathname) == 0) {
+  (void)flags; (void)mode;
+  for (int i = 0; i < NR_FILES; i++) {
+    if (file_table[i].name && strcmp(file_table[i].name, pathname) == 0) {
       file_offset[i] = 0;
-      return (int)i;
+      return i;
     }
   }
   panic("fs_open: file %s not found", pathname);
@@ -56,31 +55,27 @@ int fs_open(const char *pathname, int flags, int mode) {
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
-  assert(fd >= 0 && fd < (int)NR_FILES);
-  if (fd == FD_STDIN || fd == FD_STDOUT || fd == FD_STDERR || len == 0) {
-    return 0;
-  }
+  assert(fd >= 0 && fd < NR_FILES);
+  if (len == 0) return 0;
+  if (fd == FD_STDIN || fd == FD_STDOUT || fd == FD_STDERR) return 0;
 
   Finfo *f = &file_table[fd];
   ReadFn reader = f->read ? f->read : ramdisk_read;
-  size_t offset = file_offset[fd];
-  size_t req = len;
+  size_t off = file_offset[fd];
 
-  if (reader == ramdisk_read && f->size > 0) {
-    if (offset >= f->size) {
-      return 0;
-    }
-    size_t avail = f->size - offset;
-    if (req > avail) req = avail;
+  if (f->size > 0) {
+    if (off >= f->size) return 0;
+    size_t avail = f->size - off;
+    if (len > avail) len = avail;
   }
 
-  size_t ret = reader(buf, f->disk_offset + offset, req);
+  size_t ret = reader(buf, f->disk_offset + off, len);
   file_offset[fd] += ret;
   return ret;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
-  assert(fd >= 0 && fd < (int)NR_FILES);
+  assert(fd >= 0 && fd < NR_FILES);
   if (len == 0) return 0;
 
   if (fd == FD_STDOUT || fd == FD_STDERR) {
@@ -92,45 +87,36 @@ size_t fs_write(int fd, const void *buf, size_t len) {
 
   Finfo *f = &file_table[fd];
   WriteFn writer = f->write ? f->write : ramdisk_write;
-  size_t offset = file_offset[fd];
-  size_t req = len;
+  size_t off = file_offset[fd];
 
-  if (writer == ramdisk_write && f->size > 0) {
-    if (offset >= f->size) {
-      return 0;
-    }
-    size_t avail = f->size - offset;
-    if (req > avail) req = avail;
+  if (f->size > 0) {
+    if (off >= f->size) return 0;
+    size_t avail = f->size - off;
+    if (len > avail) len = avail;
   }
 
-  size_t ret = writer(buf, f->disk_offset + offset, req);
+  size_t ret = writer(buf, f->disk_offset + off, len);
   file_offset[fd] += ret;
   return ret;
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence) {
-  assert(fd >= 0 && fd < (int)NR_FILES);
-  if (fd == FD_STDIN || fd == FD_STDOUT || fd == FD_STDERR) {
-    return 0;
-  }
+  assert(fd >= 0 && fd < NR_FILES);
+  if (fd == FD_STDIN || fd == FD_STDOUT || fd == FD_STDERR) return 0;
 
-  size_t new_off = 0;
+  size_t new_off;
   switch (whence) {
     case SEEK_SET: new_off = offset; break;
     case SEEK_CUR: new_off = file_offset[fd] + offset; break;
     case SEEK_END: new_off = file_table[fd].size + offset; break;
     default: panic("fs_lseek: invalid whence %d", whence);
   }
-
-  if (file_table[fd].size > 0) {
-    assert(new_off <= file_table[fd].size);
-  }
-
+  if (file_table[fd].size > 0) assert(new_off <= file_table[fd].size);
   file_offset[fd] = new_off;
   return new_off;
 }
 
 int fs_close(int fd) {
-  assert(fd >= 0 && fd < (int)NR_FILES);
+  assert(fd >= 0 && fd < NR_FILES);
   return 0;
 }
