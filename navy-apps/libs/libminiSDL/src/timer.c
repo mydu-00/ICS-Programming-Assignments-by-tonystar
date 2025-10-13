283dcc0350422d00066f8525493a0e1651eab1ac
tracer-ics2025 <tracer@njuics.org>
2025-10-13 21:58:06 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:58:06 up  1:56,  1 user,  load average: 0.61, 0.89, 1.24

--- navy-apps/libs/libminiSDL/src/timer.c
+++ navy-apps/libs/libminiSDL/src/timer.c
@@ -2,7 +2,6 @@
 #include <sdl-timer.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <unistd.h>
 
 static int warned_addtimer = 0;
 static int warned_rmtimer = 0;
@@ -13,7 +12,7 @@ SDL_TimerID SDL_AddTimer(uint32_t interval, SDL_NewTimerCallback callback, void 
     fprintf(stderr, "[miniSDL] SDL_AddTimer() not implemented; timer request ignored\n");
     warned_addtimer = 1;
   }
-  return (SDL_TimerID)0; // indicate failure/not implemented
+  return (SDL_TimerID)0;
 }
 
 int SDL_RemoveTimer(SDL_TimerID id) {
@@ -22,13 +21,17 @@ int SDL_RemoveTimer(SDL_TimerID id) {
     fprintf(stderr, "[miniSDL] SDL_RemoveTimer() not implemented\n");
     warned_rmtimer = 1;
   }
-  return 0; // not implemented
+  return 0;
 }
 
 uint32_t SDL_GetTicks() {
+  // SDL’s requirement: ms since SDL_Init(); NDL_GetTicks() already does that.
   return NDL_GetTicks();
 }
 
 void SDL_Delay(uint32_t ms) {
-  usleep((useconds_t)ms * 1000);
+  uint32_t start = NDL_GetTicks();
+  while ((NDL_GetTicks() - start) < ms) {
+    // busy-wait
+  }
 }
