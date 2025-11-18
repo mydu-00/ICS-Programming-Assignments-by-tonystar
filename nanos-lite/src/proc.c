3f1fc425ed32d5cecbe3240e3540531e384e12e9
tracer-ics2025 <tracer@njuics.org>
2025-11-18 17:39:46 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:39:46 up  2:12,  1 user,  load average: 1.00, 0.90, 1.08

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -36,6 +36,13 @@ static inline Context *context_kload(PCB *p, void (*entry)(void *), void *arg) {
 // 声明 new_page()
 extern void *new_page(size_t nr_page);
 
+#ifndef MAX_ARG
+#define MAX_ARG 256
+#endif
+#ifndef MAX_ENV
+#define MAX_ENV 256
+#endif
+
 // 加载用户程序并创建用户上下文；在“新分配”的用户栈上布置 argc/argv/envp，并把 argc 的地址放入 GPRx
 Context *context_uload(PCB *p, const char *filename,
                        char *const argv[], char *const envp[]) {
@@ -47,27 +54,29 @@ Context *context_uload(PCB *p, const char *filename,
   Area kstack = (Area){ p->stack, p->stack + sizeof(p->stack) };
   p->cp = ucontext(NULL, kstack, (void *)entry);
 
-  // 统计参数数量（必须以 NULL 结尾）
-  size_t argc = 0, envc = 0, i = 0;
+  // 计数
+  size_t argc = 0, envc = 0;
   if (argv) while (argv[argc]) argc++;
   if (envp) while (envp[envc]) envc++;
 
-  // 为用户栈分配 32KB，新用户栈顶
-  char *ustack_base = (char *)new_page(8);
+  if (argc > MAX_ARG) panic("argc=%zu exceeds MAX_ARG=%d", argc, MAX_ARG);
+  if (envc > MAX_ENV) panic("envc=%zu exceeds MAX_ENV=%d", envc, MAX_ENV);
+
+  // 分配新用户栈
+  char *ustack_base = (char *)new_page(8);  // 32KB
   char *sp = ustack_base + 8 * 4096;
 
-  // 先拷贝字符串到栈顶向下
-  char *argv_ptrs[64];
-  char *envp_ptrs[64];
-  assert(argc < 64 && envc < 64);
+  // 拷贝字符串
+  char *argv_ptrs[MAX_ARG];
+  char *envp_ptrs[MAX_ENV];
 
-  for (i = 0; i < argc; i++) {
+  for (size_t i = 0; i < argc; i++) {
     size_t len = strlen(argv[i]) + 1;
     sp -= len;
     memcpy(sp, argv[i], len);
     argv_ptrs[i] = sp;
   }
-  for (i = 0; i < envc; i++) {
+  for (size_t i = 0; i < envc; i++) {
     size_t len = strlen(envp[i]) + 1;
     sp -= len;
     memcpy(sp, envp[i], len);
@@ -77,21 +86,19 @@ Context *context_uload(PCB *p, const char *filename,
   // 指针对齐
   sp = (char *)((uintptr_t)sp & ~(sizeof(uintptr_t) - 1));
 
-  // 分配指针数组区: [argc][argv...][NULL][envp...][NULL]
+  // 构造指针区 [argc][argv...][NULL][envp...][NULL]
   size_t nwords = 1 + argc + 1 + envc + 1;
   sp -= nwords * sizeof(uintptr_t);
   uintptr_t *args = (uintptr_t *)sp;
 
   args[0] = (uintptr_t)argc;
   uintptr_t idx = 1;
-  for (i = 0; i < argc; i++) args[idx++] = (uintptr_t)argv_ptrs[i];
+  for (size_t i = 0; i < argc; i++) args[idx++] = (uintptr_t)argv_ptrs[i];
   args[idx++] = 0;
-  for (i = 0; i < envc; i++) args[idx++] = (uintptr_t)envp_ptrs[i];
+  for (size_t i = 0; i < envc; i++) args[idx++] = (uintptr_t)envp_ptrs[i];
   args[idx++] = 0;
 
-  // 将 argc 的地址放到 GPRx；_start 会设置 sp=GPRx，并把该指针传给 call_main
   p->cp->GPRx = (uintptr_t)args;
-
   return p->cp;
 }
 
