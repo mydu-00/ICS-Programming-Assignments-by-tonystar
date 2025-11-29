07e584953e2d91bb2f1abe89eaeb7e0d49890a1e
tracer-ics2025 <tracer@njuics.org>
2025-11-29 22:51:58 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:51:58 up  1:57,  1 user,  load average: 0.79, 0.52, 0.44

--- navy-apps/scripts/riscv32.mk
+++ navy-apps/scripts/riscv32.mk
@@ -1,3 +1,9 @@
 include $(NAVY_HOME)/scripts/riscv/common.mk
-CFLAGS  += -march=rv32g -mabi=ilp32  #overwrite
+
+# 如果上层传了 VME=1，则加上 HAS_VME 宏
+ifeq ($(VME),1)
+CFLAGS  += -DHAS_VME
+endif
+
+CFLAGS  += -march=rv32g -mabi=ilp32  # overwrite
 LDFLAGS += -melf32lriscv
