270230162f588f808bedb08447896e0079414002
tracer-ics2025 <tracer@njuics.org>
2026-01-19 11:43:00 +0800

    >  compile NEMU 231180166 杜明阳 Linux tonystar-linux 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:43:00 up 17 min,  1 user,  load average: 0.59, 0.83, 0.73

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,7 +28,7 @@ void init_proc() {
   Log("Initializing processes...");
 
   /* load program here */
-  naive_uload(current, "/bin/nterm");
+  naive_uload(current, "/bin/pal");
 }
 
 Context* schedule(Context *prev) {
